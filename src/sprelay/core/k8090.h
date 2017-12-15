97cfb8338c7d44cd69b1a815188420dd5a3df1a0
anonym <peter.kubascik31@gmail.com>
2017-12-15 10:46:17 +0100

    New design.

--- src/sprelay/core/k8090.h
+++ src/sprelay/core/k8090.h
@@ -24,7 +24,9 @@
 #define SPRELAY_CORE_K8090_H_
 
 #include <QObject>
-#include <bits/stdc++.h>  // For priority queue.
+#include<QTimer>
+#include<queue>
+#include<vector>
 
 // forward declarations
 class QSerialPort;
@@ -52,7 +54,7 @@ enum class Command {
 inline Command& operator++(Command &cmd) { cmd = static_cast<Command>(static_cast<int>(cmd) + 1); return cmd; }
 inline Command operator++(Command &cmd, int) { Command tmp(cmd); cmd++; return tmp; }
 
-enum struct RelayID : unsigned char
+enum struct Relays : unsigned char
 {
     None  = 0,
     One   = 1 << 0,
@@ -65,13 +67,13 @@ enum struct RelayID : unsigned char
     Eight = 1 << 7
 };
 
-inline constexpr RelayID operator|(RelayID a, RelayID b) {
-    return static_cast<RelayID>(static_cast<unsigned char>(a) | static_cast<unsigned char>(b)); }
-inline constexpr RelayID operator&(RelayID a, RelayID b) {
-    return static_cast<RelayID>(static_cast<unsigned char>(a) & static_cast<unsigned char>(b)); }
-inline RelayID operator~(RelayID a) { return static_cast<RelayID>(~static_cast<unsigned char>(a)); }
-inline RelayID& operator|=(RelayID& a, RelayID b) { return a = (a | b); }
-inline RelayID& operator&=(RelayID& a, RelayID b) { return a = (a & b); }
+inline constexpr Relays operator|(Relays a, Relays b) {
+    return static_cast<Relays>(static_cast<unsigned char>(a) | static_cast<unsigned char>(b)); }
+inline constexpr Relays operator&(Relays a, Relays b) {
+    return static_cast<Relays>(static_cast<unsigned char>(a) & static_cast<unsigned char>(b)); }
+inline Relays operator~(Relays a) { return static_cast<Relays>(~static_cast<unsigned char>(a)); }
+inline Relays& operator|=(Relays& a, Relays b) { return a = (a | b); }
+inline Relays& operator&=(Relays& a, Relays b) { return a = (a & b); }
 
 struct ComPortParams
 {
@@ -94,9 +96,34 @@ public:  // NOLINT(whitespace/indent)
 
 
 signals:  // NOLINT(whitespace/indent)
-
+        void relayStatus(unsigned char previous, unsigned char current, unsigned char timed);
+        void buttonStatus(unsigned char state, unsigned char pressed, unsigned char released);
+        void timerDelay(unsigned char relay, int delay);
+        void buttonMode(unsigned char momentary, unsigned char toggle, unsigned char timed);
+        void jumperStatus(bool on);
+        void firmwareVersion(int year, int week);
+        void connected();
 public slots:  // NOLINT(whitespace/indent)
+    void onFailedAttemptForConnection();
     void connectK8090();
+    void connection();
+    void disconnect();
+    void switchRelayOn(K8090Traits::Relays relays);
+    void switchRelayOff(K8090Traits::Relays relays);
+    void toggleRelay(K8090Traits::Relays relays);
+    void setButtonMode(K8090Traits::Relays momentary, K8090Traits::Relays toggle, K8090Traits::Relays timed);
+    void startRelayTimer(K8090Traits::Relays relays, unsigned int delay);
+    void setRelayTimerDelay(K8090Traits::Relays relays, quint16 delay);
+    void queryRelayStatus();
+    void queryRemainingTimerDelay(K8090Traits::Relays relays);
+    void queryTotalTimerDelay(K8090Traits::Relays relays);
+    void queryButtonModes();
+    void resetFactoryDefauts();
+    void queryJumperStatus();
+    void queryFirmwareVersion();
+    void refreshRelayStates(unsigned char previous, unsigned char current, unsigned char timed);
+    void refreshButtonMode(unsigned char momentary, unsigned char toggle, unsigned char timed);
+
 
 protected slots:  // NOLINT(whitespace/indent)
     void onSendToSerial(const unsigned char *buffer, int n);
@@ -104,32 +131,7 @@ protected slots:  // NOLINT(whitespace/indent)
 private slots:  // NOLINT(whitespace/indent)
     void onReadyData();
 
-private:  // NOLINT(whitespace/indent)
-    unsigned char choose(bool Relays[8]);
-    void sendCommand(int param);
-    void sendCommand(bool Relays[8], int param);
-    void sendCommand(bool Relays[8], int param, unsigned int Time);
-    void sendCommand(bool Relays[8], int param,  bool option, bool notused);
-    void sendCommand(bool Relays1[8], bool Relays2[8], bool Relays3[8], int param, bool notused);
-    void startWork();
-
-    void sendSwitchRelayOnCommand(unsigned char chosen);
-    void sendSwitchRelayOffCommand(unsigned char chosen);
-    void sendtoggleRelayCommand(unsigned char chosen);
-    void sendsetButtonMode(unsigned char choose1, unsigned char choose2, unsigned char choose3);
-    void sendStartRelayTimer(unsigned char chosen, unsigned int Time);
-    void sendSetRelayTimer(unsigned char chosen, unsigned int Time);
-    void sendQueryTimerDelay(unsigned char chosen, unsigned char option);
-    void sendQueryButtonMode();
-    void sendQueryRelayStatus();
-    void sendButtonStatus();
-    void sendRelayStatus();
-    void sendRessetfactorydefaults();
-    void sendJumperStatus();
-    void sendFirmwareVersion();
-    void completedTaskControl();
-    void completedTaskControl(int Time);
-
+private:  // NOLINT(whitespace/indent)    
     unsigned char lowByt(unsigned int number);
     unsigned char highByt(unsigned int number);
     static const quint16 productID;
@@ -146,22 +148,32 @@ private:  // NOLINT(whitespace/indent)
     static bool validateResponse(const unsigned char *bMsg, int n, K8090Traits::Command cmd);
 
     static void fillCommandsArrays();
-    struct members_of_list  // variables of this type
-    {                       // will be used in list. According to priority, list will be sorted.
-        QString info_;
-        unsigned char  * cmd_;
-        int priority_;
-        int time_ = 0;
-        bool operator<(const members_of_list& a) const
-        {
-            return priority_< a.priority_;
-        }
-    };
-    std::priority_queue< members_of_list, std::vector<members_of_list>> storedcommands_;
+
 
     bool commandBuffer[static_cast<int>(K8090Traits::Command::None)];
 
     K8090Traits::Command lastCommand;
+
+    unsigned char relay_states_ = 0;
+    unsigned char momentary_button_mode_ = 0;
+    unsigned char toggle_button_mode_ = 0;
+    unsigned char timed_button_mode_ = 0;
+    bool command_finished_ = true;
+
+    QTimer *timer_ = new QTimer;
+    int number_of_failed_attemps_for_connection_ = 0;
+    struct stored_command_structure
+{
+    unsigned char *cmd;
+    int priority;
+    bool operator<(const stored_command_structure& a)const
+    {
+        return priority < a.priority;
+    }
+    };
+
+    std::priority_queue<stored_command_structure, std::vector<stored_command_structure>> stored_command_priority_queue;
+
 
     static unsigned char bCommands[static_cast<int>(K8090Traits::Command::None)][2];
     static unsigned char bEtxByte;
