458e5282ada57d7c7bc2a2e870716edffe5deabb
lumik <lumiksro@centrum.cz>
2018-11-15 11:29:43 +0100

    Changed commands and responses to std::array.

--- src/biomolecules/sprelay/core/k8090_commands.h
+++ src/biomolecules/sprelay/core/k8090_commands.h
@@ -37,6 +37,8 @@
 #ifndef BIOMOLECULES_SPRELAY_CORE_K8090_COMMANDS_H_
 #define BIOMOLECULES_SPRELAY_CORE_K8090_COMMANDS_H_
 
+#include <array>
+
 #include "k8090_defines.h"
 
 namespace biomolecules {
@@ -46,6 +48,107 @@ namespace k8090 {
 namespace impl_ {
 
 // generate static arrays containing commands, command priorities and responses at compile time
+
+
+// tried to make the static array creation more modern but gcc 4.9 does not treat constexpr correctly
+// so revert to the old way.
+// // generator functions
+//constexpr unsigned char make_binary_command(std::size_t id) noexcept
+//{
+//    return
+//        // clang-format off
+//        id == as_number(CommandID::RelayOn) ?  0x11 :
+//        id == as_number(CommandID::RelayOff) ? 0x12 :
+//        id == as_number(CommandID::ToggleRelay) ? 0x14 :
+//        id == as_number(CommandID::QueryRelay) ? 0x18 :
+//        id == as_number(CommandID::SetButtonMode) ? 0x21 :
+//        id == as_number(CommandID::ButtonMode) ? 0x22 :
+//        id == as_number(CommandID::StartTimer) ? 0x41 :
+//        id == as_number(CommandID::SetTimer) ? 0x42 :
+//        id == as_number(CommandID::Timer) ? 0x44 :
+//        id == as_number(CommandID::ResetFactoryDefaults) ? 0x66 :
+//        id == as_number(CommandID::JumperStatus) ? 0x70 :
+//        id == as_number(CommandID::FirmwareVersion) ? 0x71 :
+//        0;
+//    // clang-format on
+//}
+
+//constexpr int make_command_priority(std::size_t id) noexcept
+//{
+//    return
+//        // clang-format off
+//        id == as_number(CommandID::RelayOn) ? 1 :
+//        id == as_number(CommandID::RelayOff) ? 1 :
+//        id == as_number(CommandID::ToggleRelay) ? 1 :
+//        id == as_number(CommandID::QueryRelay) ? 2 :
+//        id == as_number(CommandID::SetButtonMode) ? 1 :
+//        id == as_number(CommandID::ButtonMode) ? 2 :
+//        id == as_number(CommandID::StartTimer) ? 1 :
+//        id == as_number(CommandID::SetTimer) ? 1 :
+//        id == as_number(CommandID::Timer) ? 2 :
+//        id == as_number(CommandID::ResetFactoryDefaults) ? 1 :
+//        id == as_number(CommandID::JumperStatus) ? 1 :
+//        id == as_number(CommandID::FirmwareVersion) ? 1 :
+//        0;
+//    // clang-format on
+//}
+
+//constexpr unsigned char make_binary_response(std::size_t id) noexcept
+//{
+//    return
+//        // clang-format off
+//        id == as_number(ResponseID::ButtonMode) ? 0x22 :
+//        id == as_number(ResponseID::Timer) ? 0x44 :
+//        id == as_number(ResponseID::ButtonStatus) ? 0x50 :
+//        id == as_number(ResponseID::RelayStatus) ? 0x51 :
+//        id == as_number(ResponseID::JumperStatus) ? 0x70 :
+//        id == as_number(ResponseID::FirmwareVersion) ? 0x71 :
+//        0;
+//    // clang-format on
+//}
+
+// // helper structures
+
+//template<std::size_t...>
+//struct IndexSequence
+//{};
+
+//template<std::size_t tCounter, std::size_t... tRest>
+//struct IndexSequenceGenerator : IndexSequenceGenerator<tCounter - 1, tCounter - 1, tRest...>
+//{};
+
+// // zero case
+//template<std::size_t... tRest>
+//struct IndexSequenceGenerator<0u, tRest...>
+//{
+//    using type = IndexSequence<tRest...>;
+//};
+
+//template<std::size_t N>
+//using MakeIndexSequence = typename IndexSequenceGenerator<N>::type;
+
+//template<typename T, std::size_t... tIndexes, std::size_t tSize = sizeof...(tIndexes)>
+//constexpr std::array<T, tSize> make_value_array_from_seq(
+//    T (*func)(std::size_t), IndexSequence<tIndexes...> /*unused*/) noexcept
+//{
+//    return {func(tIndexes)...};
+//}
+
+// // functions that make the arrays
+//constexpr std::array<unsigned char, as_number(CommandID::None)> make_command_array() noexcept
+//{
+//    return make_value_array_from_seq(&make_binary_command, MakeIndexSequence<as_number(CommandID::None)>{});
+//}
+
+//constexpr std::array<int, as_number(CommandID::None)> make_command_priority_array() noexcept
+//{
+//    return make_value_array_from_seq(&make_command_priority, MakeIndexSequence<as_number(CommandID::None)>{});
+//}
+
+//constexpr std::array<unsigned char, as_number(ResponseID::None)> make_response_array() noexcept
+//{
+//    return make_value_array_from_seq(&make_binary_response, MakeIndexSequence<as_number(ResponseID::None)>{});
+//}
 
 // template function to fill the array with appropriate commands and priorities
 template<unsigned int N>
@@ -167,7 +270,7 @@ template<typename T, T... Args>
 struct XArrayData
 {
     // initializing declaration
-    static constexpr T kValues[sizeof...(Args)] = {Args...};
+    static constexpr std::array<T, sizeof...(Args)> kValues{Args...};
 };
 
 // recursively generates command typedefs
@@ -219,22 +322,25 @@ struct ResponseArray_
 
 // static const array definition (needed to create the static array kValues to satisfy ODR, deprecated c++17)
 template<typename T, T... Args>
-constexpr T XArrayData<T, Args...>::kValues[sizeof...(Args)];
+constexpr std::array<T, sizeof...(Args)> XArrayData<T, Args...>::kValues;
 
 /*!
  * \brief Array of hexadecimal representation of commands used to control the relay.
  */
-constexpr const unsigned char* kCommands = CommandArray_<as_number(CommandID::None)>::Commands::kValues;
+constexpr std::array<unsigned char, as_number(CommandID::None)> kCommands =
+    CommandArray_<as_number(CommandID::None)>::Commands::kValues;
 
 /*!
  * \brief Array of default priorities used to command scheduling.
  */
-constexpr const int* kPriorities = CommandArray_<as_number(CommandID::None)>::Priorities::kValues;
+constexpr std::array<int, as_number(CommandID::None)> kPriorities =
+    CommandArray_<as_number(CommandID::None)>::Priorities::kValues;
 
 /*!
  * \brief Array of hexadecimal representation of responses sended by the relay.
  */
-constexpr const unsigned char* kResponses = ResponseArray_<as_number(ResponseID::None)>::Responses::kValues;
+constexpr std::array<unsigned char, as_number(ResponseID::None)> kResponses =
+    ResponseArray_<as_number(ResponseID::None)>::Responses::kValues;
 
 /*!
  * \brief Start delimiting command byte.
